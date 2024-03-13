cbd11f7021e7be035e8c3094a4248309e5ac1ff8
Rahul Khanal <rahul39khanal@gmail.com>
2024-03-13 22:00:17 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config(); // Load environment variables from .env file
+const express = require("express");
+const app = new express();
+// const { Pool } = require("pg");
+// const mongoose = require("mongoose");
+
+// try {
+//   const pool = new Pool({
+//     user: process.env.PGUSER,
+//     host: process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT,
+//   });
+// } catch (error) {
+//   throw error;
+// }
+
+// try {
+//   mongoose.connect(
+//     `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`
+//   );
+// } catch (error) {
+//   throw error;
+// }
+app.get("/", (req, resp) => {
+  resp.send("I must run.");
+});
+
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`connected in port ${process.env.PORT}....`);
+});
